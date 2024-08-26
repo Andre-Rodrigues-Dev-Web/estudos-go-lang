409a3603e630648e45f63dac8ffc5c09fc1062bb
André Rodrigues <79713707+Andre-Rodrigues-Dev-Web@users.noreply.github.com>
2024-08-26 10:26:30 -0300

    feat: adicionado arquivos de estudos

---
+++ http/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		path := filepath.Join("views", "index.html")
+		http.ServeFile(w, r, path)
+	})
+
+	log.Println("Starting server on :8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
